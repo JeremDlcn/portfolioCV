9799d8589656f9ad5c6d07679543cd97d33a9149
JeremDlcrn <44464165+JeremDlcrn@users.noreply.github.com>
2020-01-20 23:34:28 +0100

    Filling Database

--- data.js
+++ data.js
@@ -3,41 +3,45 @@ const projet = [
 		title: 'Cow-snack',
 		category: 'Site web',
 		link: 'https://cow-snack.fr',
-		date: '12 Février 2020',
-		description: 'lorem ipsum',
+		date: 'Octobre 2019',
+		description: 'Cow-Snack est un site de commande de sandwich personnalisé crée avec deux autres étudiants dans le but d\'en apprendre plus sur le Javascript, le Node.js et le déploiement d\'un site sur un serveur, ce projet apportait une nouvelle expérience pour nous car nous avions jamais déployé un site sur unserveur en s\'occupant de tout la partie sécurité en plus des nouvelles technologies que nous avons utilisés pourcréer le site.',
 		techno: [
 			{
-				img: 'js.png',
-				name: 'Javascript'
+				img: 'adobexd.png',
+				name: 'AdobeXD'
+			},
+			{
+				img: 'htmlcss.png',
+				name: 'HTML/CSS'
 			},
 			{
 				img: 'js.png',
 				name: 'Javascript'
 			},
 			{
-				img: 'js.png',
-				name: 'Javascript'
+				img: 'nodejs.png',
+				name: 'Node.js'
+			},
+			{
+				img: 'nginx.png',
+				name: 'Nginx'
+			},
+			{
+				img: 'mysql.png',
+				name: 'MySQL'
 			}
 		]
 	},
 	{
 		title: 'DailyUI',
 		category: 'UI / UX Design',
-		link: 'https://cow-snack.fr',
-		date: '12 Février 2020',
-		description: 'lorem ipsum',
+		link: 'https://twitter.com/JeremDlcn',
+		date: 'Novembre 2019',
+		description: 'DailyUI est un challenge de 100jours pendant lesquelles je dois réaliser 100 interfaces/créations graphiques sur un élément imposé, pouvant aller d\'un simple icône à un landing page et les sujets sont variés et me permettent de sortir de ma zone de confort en créant des interfaces originales. Ce challenge me permet de prendre du niveau en design d\'interface tout en m\'obligeant à garder un niveau de qualité pour chacune de mes créations.',
 		techno: [
 			{
-				img: 'js.png',
-				name: 'Javascript'
-			},
-			{
-				img: 'js.png',
-				name: 'Javascript'
-			},
-			{
-				img: 'js.png',
-				name: 'Javascript'
+				img: 'adobexd.png',
+				name: 'AdobeXD'
 			}
 		]
 
@@ -45,38 +49,43 @@ const projet = [
 	{
 		title: 'Univership',
 		category: 'Site web',
-		link: 'https://cow-snack.fr',
-		date: '12 Février 2020',
-		description: 'lorem ipsum',
+		link: 'https://univership.000webhostapp.com/',
+		date: 'Janvier 2020',
+		description: 'Univership est un projet étudiant de jeu vidéo spacial à monde ouvert, ma mission était de leur fournir un site web afin de présenter leur futur jeu, écrire des articles par rapport à l\'avancé du développement du jeu ou encore de futur mise à jour, le projet n\'en ai qu\'à ses débuts, cela entraîne des problèmes de temps, il manque donc des images en rapport avec le jeu pour avoir le site fini.',
 		techno: [
 			{
-				img: 'js.png',
-				name: 'Javascript'
+				img: 'adobexd.png',
+				name: 'AdobeXD'
+			},
+			{
+				img: 'htmlcss.png',
+				name: 'HTML/CSS'
 			},
 			{
 				img: 'js.png',
 				name: 'Javascript'
 			},
 			{
-				img: 'js.png',
-				name: 'Javascript'
-			}
+				img: 'nodejs.png',
+				name: 'Node.js'
+			},
+			{
+				img: 'mongodb.png',
+				name: 'MongoB'
+			},
+
 		]
 	},
 	{
 		title: 'Smile',
-		category: 'Site web',
-		link: 'https://cow-snack.fr',
-		date: '12 Février 2020',
-		description: 'lorem ipsum',
+		category: 'Jeu de Société & Site Web',
+		link: 'https://smile-nantes.000webhostapp.com/',
+		date: 'Novembre 2019 - Janvier 2020',
+		description: 'Smile est une équipe d\étudiant ayant pour mission d\'expliquer les méthodes agiles de manière ludique à travers un jeu de société. Ce jeu de plateau fait s\'affronter 2 équipes: la première équipe représente les méthodes "classiques" de gestion de projet et la seconde représente les méthodes agiles. A chaque tour les équipes devront avancer afin de réaliser des activités pour gagner un maximum de points auprès du client, ce client décidera si l\'activité à bien été réaliser et si elle convient à ses attentes. Chaque équipe possèdent un spécificité, l\'équipe des méthodes "classiques" doit poser tout leur questions au client avant de pouvoir commencer l\'activité alors que les méthodes agiles peuvent poser des questions tout au long de l\'activité. J\'ai réalisé le site web servant au activités, les cartes et aidé dans la création des règles du jeu.',
 		techno: [
 			{
-				img: 'js.png',
-				name: 'Javascript'
-			},
-			{
-				img: 'js.png',
-				name: 'Javascript'
+				img: 'htmlcss.png',
+				name: 'HTML/CSS'
 			},
 			{
 				img: 'js.png',
@@ -85,44 +94,56 @@ const projet = [
 		]
 	},
 	{
-		title: 'Blog',
-		category: 'Site web',
-		link: 'https://cow-snack.fr',
-		date: '12 Février 2020',
-		description: 'lorem ipsum',
+		title: 'TriCycle',
+		category: 'Application Mobile',
+		link: 'https://twitter.com/TriCycle_app',
+		date: 'Novembre 2019',
+		description: 'TriCycle est une application permettant de scanner les codes barres de produits afin de savoir comment les recycler, aider les personnes a comprendre le fonctionnement de la chaîne de tri, aidé les gens à avoir des gestes plus écologiques ,proposer des alternatives moins couteuses et plus écologiques, avoir un regard positif sur l\'écologie',
 		techno: [
 			{
-				img: 'js.png',
-				name: 'Javascript'
+				img: 'adobexd.png',
+				name: 'AdobeXD'
 			},
 			{
-				img: 'js.png',
-				name: 'Javascript'
+				img: 'htmlcss.png',
+				name: 'HTML/CSS'
 			},
 			{
-				img: 'js.png',
-				name: 'Javascript'
-			}
+				img: 'ionic.png',
+				name: 'Ionic'
+			},
+			{
+				img: 'nodejs.png',
+				name: 'Node.js'
+			},
+			{
+				img: 'nginx.png',
+				name: 'Nginx'
+			},
+			{
+				img: 'mongodb.png',
+				name: 'Mongodb'
+			}		
 		]
 	},
 	{
 		title: 'Voilier Handicap',
-		category: 'Site web',
-		link: 'https://cow-snack.fr',
-		date: '12 Février 2020',
-		description: 'lorem ipsum',
+		category: 'IoT & Site Web',
+		link: 'https://voilierhandicap.000webhostapp.com/',
+		date: 'Mars 2018',
+		description: 'Le projet "Voilier Handicap" est un projet de Terminale visant à adapter un voilier de course pour l\'adapter aux personnes à mobilité réduite. Nous avons crée un système de reconnaissance de mouvement permettant aux personnes à mobilité réduites de maneuvrés le voilier à distance, d\'autre part j\'ai crée une application mobile permettant à l\'accompagnateur de commander et sécuriser le voilier, on peut y voir la vitesse du voilier en temps réel et monter ou descendre la Grand-Voile tout en restant en cabine.',
 		techno: [
 			{
-				img: 'js.png',
-				name: 'Javascript'
+				img: 'htmlcss.png',
+				name: 'HTML/CSS'
 			},
 			{
-				img: 'js.png',
-				name: 'Javascript'
+				img: 'java.png',
+				name: 'Java'
 			},
 			{
-				img: 'js.png',
-				name: 'Javascript'
+				img: 'arduino.png',
+				name: 'Arduino'
 			}
 		]
 	}
